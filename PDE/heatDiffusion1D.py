231ccaf38951d7a53e241d7135ec75cc0e0014f8
Domcikas <Domcikas@gmail.com>
2019-05-13 02:54:58 +0300

    Update heatDiffusion1D.py

--- PDE/heatDiffusion1D.py
+++ PDE/heatDiffusion1D.py
@@ -6,7 +6,7 @@ length = 1
 numberOfNodes = 50
 leftBoundary = 2
 rightBoundary = 0.5
-finalTime = 15
+finalTime = 60
 
 D0 = np.ones(numberOfNodes)*0.1
 energyS = np.zeros(numberOfNodes)
@@ -21,6 +21,32 @@ def calcTimeStep(dx, D):
 
 def calcTimeStep2D(dx,dy,D):
 	return ((dx**2)*(dy**2))/(2*D*((dx**2)+(dy**2)))
+
+def calcTimeStepPolar(r, nS, n, D):
+	dr = r/n
+	#trapezoid points
+	#v1
+	x1 = r
+	y1 = 0
+	#v2
+	x2 = r-dr
+	y2 = 0
+	#v3
+	x3 = (r-dr)*cos(2*np.pi*1/nS)
+	y3 = (r-dr)*sin(2*np.pi*1/nS)
+	#v4
+	x4 = r*cos(2*np.pi*1/nS)
+	y4 = r*sin(2*np.pi*1/nS)
+	#distances
+	v2v3 = np.sqrt((x2-x3)**2+(y2-y3)**2)
+	v1v4 = np.sqrt((x1-x4)**2+(y1-y4)**2)
+	v1v2 = np.sqrt((x1-x2)**2+(y1-y2)**2)
+	h = np.sqrt(v1v2**2-((v1v4-v2v3)/2)**2)
+	diameter = np.sqrt(h**2+(v2v3+(v1v4-v2v3)/2)**2)
+	print 0.001*diameter/D
+	return 0.001*diameter/D
+
+		
 
 def internalHeatSource(x, sourceExists):
 	return 2*sourceExists
@@ -256,18 +282,24 @@ def FTCSmethod2D(lengthX, lengthY, numberOfNodesX, numberOfNodesY, initMatrix, l
 def FTCSmethod2DPolar(R, numberOfNodes, numberOfSegments, initMatrix, boundary, thermalConst, finalTime):
 	#Getting the parameters
 	n = numberOfNodes
+	nS = numberOfSegments
+	#Temperature / chemical #1 initialization
 	T = initMatrix
+	#Temperature flow / chemical intake
 	TS = boundary
+	#Chemical #2 initialization
+	Ch2 = np.zeros((nS,n+1))
 	dR = float(R)/float(n)
-	rad = np.linspace(dR/2, R-dR/2, n)
-	azm = np.linspace(0, 2*np.pi, numberOfSegments)
+	#rad = np.linspace(dR/2, R-dR/2, n)
+	rad = np.linspace(0, R, n+1)
+	azm = np.linspace(0, 2*np.pi, nS)
 	r, th = np.meshgrid(rad, azm)
 	t_final = finalTime
 	alpha = thermalConst
 	dPhi = (2*np.pi)/numberOfSegments
 
 	#Calculating suitable dt
-	dt = calcTimeStep2D(dR, dPhi, float(alpha))
+	dt = calcTimeStepPolar(R, nS, n, float(alpha))
 	
 	
 	#Finding minimum and maximum values of temperature
@@ -282,35 +314,54 @@ def FTCSmethod2DPolar(R, numberOfNodes, numberOfSegments, initMatrix, boundary, 
 		tMax = tMax if tMax > max(i) else max(i)
 	tMax = tMax if tMax > boundary else boundary
 
-	print tMin
-	print tMax
 
 	t = np.arange(0, t_final, dt)
-	dTdt = np.empty((numberOfSegments, n))
+	dTdt = np.empty((nS, n+1))
 
 	#Running the simulation
-	for j in range(1,len(t)):
-		temperature = []
-		for i in range(0,numberOfSegments):
+	for j in range(0,len(t)):
+		for i in range(0,nS):
 			symNode = T[(i+numberOfSegments/2)%numberOfSegments][0]
-			for k in range(1, n-1):
+			for k in range(1, n):
+###################################################################
+			#TEMPERATURE / CHEMICAL #1 DIFFUSION
+###################################################################
 				#Inner part of the slice
-				dTdt[i][k] = alpha*((-(T[i][k]-T[(i-1)%n][k])/dPhi**2+(T[(i+1)%n][k]-T[i][k])/dPhi**2)+(-(T[i][k]-T[i][k-1])/dR**2+(T[i][k+1]-T[i][k])/dR**2))
+				dTdt[i][k] = alpha*(((T[(i+1)%nS][k]-2*T[i][k]+T[(i-1)%nS][k])/((dPhi**2)*(k*dR)**2))+((T[i][k+1]-2*T[i][k]+T[i][k-1])/dR**2)+((T[i][k+1]-T[i][k-1])/(2*dPhi*k*dR))) #dTdt = alpha*((d^2 T/d^2 phi * r^2)+(d^2 T/d^2 r)+(dT/dr * r))
 			#Wide and narrow ends of the slice
-			#Narrow end of slice takes values from symmetrical slice narrow end 			
-			dTdt[i][0] = alpha*((-(T[i][0]-T[(i-1)%n][0])/dPhi**2+(T[(i+1)%n][0]-T[i][0])/dPhi**2)+(-(T[i][0]-symNode)/dR**2+(T[i][1]-T[i][0])/dR**2))
-			#Wide end of slice takes values from boundary
-			dTdt[i][n-1] = alpha*((-(T[i][n-1]-T[(i-1)%n][n-1])/dPhi**2+(T[(i+1)%n][n-1]-T[i][n-1])/dPhi**2)+(-(T[i][n-1]-T[i][n-2])/dR**2+(boundary-T[i][n-1])/dPhi**2))
-
-
+			#Zero point end of slice takes values from points in + directions 			
+			#dTdt[i][0] = alpha*((-(T[i][0]-T[(i-1)%n][0])/dPhi**2+(T[(i+1)%n][0]-T[i][0])/dPhi**2)+(-(T[i][0]-symNode)/dR**2+(T[i][1]-T[i][0])/dR**2))
+			#dTdt[i][0] =  alpha*(((T[(i+1)%n][1]-2*T[i][0]+T[(i-1)%n][0])/((dPhi**2)*(k*dR)**2))+((T[i][k+1]-2*T[i][k]+T[i][k-1])/dR**2))
+			#Angles:
+			#90 degrees - nS/4
+			#180 degrees - nS/2
+			#270 degrees - 3*nS/2
+			dTdt[i][0] = alpha*((T[0][1]-4*T[i][0]+T[nS/4][1]+T[nS/2][1]+T[3*nS/4][1])/dR**2)
+			#Boundary end of slice takes values from boundary
+			#Dirichlet boundary
+			bound = 2
+			#Sine boundary
+			#bound = 20*sin(2*np.pi*(float(i)/float(nS-1)))
+			#Neumann boundary
+			#bound = (boundary/alpha-(T[(i+1)%n][n]-2*T[i][n]+T[(i-1)%n][n])/dPhi**2)*dR**2+2*T[i][n]+T[i][n-1]
+			#print bound
+			dTdt[i][n] = alpha*(((T[(i+1)%nS][n]-2*T[i][n]+T[(i-1)%nS][n])/((dPhi**2)*(n*dR)**2))+((bound-2*T[i][n]+T[i][n-1])/dR**2)+((bound-T[i][n-1])/(2*dPhi*n*dR)))
+#######################################################################################
+			#CHEMICAL #2 DIFFUSSION 
+#######################################################################################
+			
 		T = np.add(T, dTdt*dt)
-		#temperature = T.tolist()
 		#Plotting the simulation to heatmap
-		if j:
+		if j >= len(t)-3:
+			print T
 			plt.subplot(projection="polar")
-			c = plt.pcolormesh(th,r,T)#,vmin=tMin, vmax=tMax)
+			c = plt.pcolormesh(th,r,T,vmin=tMin, vmax=tMax)
+			print tMin
+			print tMax
 			plt.plot(azm,r,color='k',ls='none')
 			plt.colorbar(c)
+			#plt.thetagrids([theta * 15 for theta in range(360//15)])
+			#plt.grid()
 			plt.show()
 			#fig, ax = plt.subplots()
 			#c = ax.pcolormesh(X,Y, T, cmap='RdBu_r', vmin=tMin, vmax=tMax)
@@ -321,9 +372,19 @@ def FTCSmethod2DPolar(R, numberOfNodes, numberOfSegments, initMatrix, boundary, 
 			#outputName = '2D_outputs/output'+"{:05d}".format(j)+'.jpg'
 			#plt.savefig(outputName, bbox_inches='tight')
 			#plt.close()
-			
-#finiteVolumeMethod(leftBoundary, rightBoundary, finalTime, length, numberOfNodes, D0, energyS)
 
+def initFunction1(r):
+	return np.e**(-(r**2)*10)
+
+
+#finiteVolumeMethod(leftBoundary, rightBoundary, finalTime, length, numberOfNodes, D0, energyS)
 #FTCSexample(leftBoundary, rightBoundary, finalTime, length, numberOfNodes, D0, energyS)
 #FTCSmethod2D(length, length, numberOfNodes, numberOfNodes, np.zeros((numberOfNodes, numberOfNodes)), leftBoundary, leftBoundary, leftBoundary, leftBoundary, 0.001, finalTime)
-FTCSmethod2DPolar(length, numberOfNodes, numberOfNodes, np.zeros((numberOfNodes, numberOfNodes)), leftBoundary, 1, finalTime)
+
+test = np.zeros((numberOfNodes, numberOfNodes+1))
+#for i in range(0,numberOfNodes):
+#	for j in range(0, numberOfNodes+1):
+#		test[i][j] = 10 if j == 0 else 0#initFunction1(length*j/numberOfNodes) 
+#test = np.zeros((numberOfNodes, numberOfNodes+1))
+print test
+FTCSmethod2DPolar(length, numberOfNodes, numberOfNodes, test, leftBoundary, 0.01, finalTime)
